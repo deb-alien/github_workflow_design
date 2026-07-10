0dfeeb9b56e58474f1eec2f18a91f1d1132e2ff2
deb-alien <dev.rahim936@gmail.com>
2026-07-10 23:30:58 +0600

    fix(terraform): Correct variable name for AWS region in CI workflow feat(outputs): Add OIDC provider ARN output to Terraform outputs

--- infra/bootstrap/outputs.tf
+++ infra/bootstrap/outputs.tf
@@ -13,6 +13,12 @@ output "github_oidc_role_arn" {
   description = "The ARN of the IAM role for GitHub Actions OIDC"
 }
 
+output "oidc_provider_arn" {
+  value       = aws_iam_openid_connect_provider.github_oidc.arn
+  description = "The ARN of the OIDC provider for GitHub Actions"
+
+}
+
 output "github_oidc_role_name" {
   value       = aws_iam_role.github_oidc_role.name
   description = "The name of the IAM role for GitHub Actions OIDC"
