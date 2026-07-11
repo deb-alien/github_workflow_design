44ac61dc1c59bbf14b0003343e7548b098c9118e
deb-alien <dev.rahim936@gmail.com>
2026-07-11 14:19:57 +0600

    feat(alb): Parameterize ALB configuration variables for internal, HTTP2, and idle timeout

--- infra/modules/alb/load_balancer.tf
+++ infra/modules/alb/load_balancer.tf
@@ -2,13 +2,13 @@ resource "aws_alb" "this" {
   name                             = "${var.project_name}-${var.environment}-alb"
   load_balancer_type               = "application"
   enable_cross_zone_load_balancing = true
-  internal                         = false
+  internal                         = var.internal
   security_groups                  = var.security_group_ids
   subnets                          = var.public_subnet_ids
   enable_deletion_protection       = var.enable_delete_protection
-  enable_http2                     = true
+  enable_http2                     = var.enable_http2
   drop_invalid_header_fields       = true
-  idle_timeout                     = 60
+  idle_timeout                     = var.idle_timeout
 
   tags = merge(
     local.common_tags,
