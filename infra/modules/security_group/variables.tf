b98a6efccd9e822bfc36e1b2cb05ddfc25b69ffe
deb-alien <dev.rahim936@gmail.com>
2026-07-11 08:17:23 +0600

    feat(security_group): inital module skeleton

---
+++ infra/modules/security_group/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment of the project (e.g., dev, staging, prod)."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the ALB will be deployed."
+  type        = string
+}
