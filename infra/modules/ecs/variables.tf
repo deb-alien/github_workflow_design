c71c44d9c6dae79897e290da095f118f79fb2beb
deb-alien <dev.rahim936@gmail.com>
2026-07-12 17:50:21 +0600

    Add ECS task definition and container definition with necessary variables

--- infra/modules/ecs/variables.tf
+++ infra/modules/ecs/variables.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  description = "The AWS region where the ECS cluster will be created."
+  type        = string
+}
+
 variable "project_name" {
   description = "The name of the project."
   type        = string
@@ -12,4 +17,36 @@ variable "container_insight" {
   description = "Enable or disable container insights for the ECS cluster."
   type        = bool
   default     = true
+}
+
+variable "execution_role_arn" {
+  description = "The ARN of the IAM role that allows ECS tasks to call AWS APIs on your behalf."
+  type        = string
+}
+
+variable "task_role_arn" {
+  description = "The ARN of the IAM role that allows ECS tasks to call AWS APIs on your behalf."
+  type        = string
+}
+
+variable "repository_url" {
+  description = "The URL of the container image repository."
+  type        = string
+}
+
+variable "image_tag" {
+  description = "The tag of the container image."
+  type        = string
+}
+
+variable "cpu" {
+  description = "The number of CPU units to reserve for the task. default is 256 (0.25 vCPU)."
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  description = "The amount of memory (in MiB) to reserve for the task. default is 512 (0.5 GiB)."
+  type        = number
+  default     = 512
 }
