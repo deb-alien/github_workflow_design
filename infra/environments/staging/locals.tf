c50da3e10aaddbbe57ec51f13cc167173282f25d
deb-alien <dev.rahim936@gmail.com>
2026-07-12 18:36:31 +0600

    Fix formatting for locals in production and staging environments

--- infra/environments/staging/locals.tf
+++ infra/environments/staging/locals.tf
@@ -1,5 +1,4 @@
 locals {
-  bootstrap_outputs = data.terraform_remote_state.bootstrap.outputs
-
-  ecr_repository_url  = local.bootstrap_outputs.ecr_repository_url
+  bootstrap_outputs  = data.terraform_remote_state.bootstrap.outputs
+  ecr_repository_url = local.bootstrap_outputs.ecr_repository_url
 }
