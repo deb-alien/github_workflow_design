058de7a5fe425c6b6a444a637d9050fa9bb300a4
deb-alien <dev.rahim936@gmail.com>
2026-07-14 14:49:08 +0600

    feat(s3-bucket): Add S3 bucket module and outputs for integration with CloudFront

--- infra/environments/staging/main.tf
+++ infra/environments/staging/main.tf
@@ -32,8 +32,9 @@ module "security_group" {
 module "iam" {
   source = "../../modules/iam"
 
-  project_name = var.project_name
-  environment  = var.environment
+  project_name  = var.project_name
+  environment   = var.environment
+  s3_bucket_arn = module.s3_bucket.s3_bucket_arn
 }
 
 module "vpc_endpoints" {
@@ -70,6 +71,75 @@ module "acm" {
 
   domain_name    = var.root_domain_name
   hosted_zone_id = module.route53.zone_id
+}
+
+module "s3_bucket" {
+  source = "../../modules/s3_bucket"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  bucket_name       = "${var.project_name}-${var.environment}-storage"
+  enable_versioning = true
+  kms_key_arn       = null
+
+  cloudfront_distribution_arn = module.cloudfront.cloudfront_distribution_arn
+  ecs_task_role_arn           = module.iam.task_role_arn
+
+  lifecycle_rules = [
+    {
+      id      = "avatars"
+      prefix  = "avatars/"
+      enabled = true
+      transition = {
+        days          = 30
+        storage_class = "STANDARD_IA"
+      }
+      expiration = {
+        days = 365
+      }
+    },
+    {
+      id      = "posts"
+      prefix  = "posts/"
+      enabled = true
+      transition = {
+        days          = 30
+        storage_class = "STANDARD_IA"
+      }
+      expiration = {
+        days = 365
+      }
+    },
+    {
+      id      = "attachments"
+      prefix  = "attachments/"
+      enabled = true
+      transition = {
+        days          = 30
+        storage_class = "STANDARD_IA"
+      }
+      expiration = {
+        days = 365
+      }
+    }
+  ]
+}
+
+module "cloudfront" {
+  source = "../../modules/cloudfront"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  bucket_name                 = module.s3_bucket.s3_bucket_name
+  bucket_regional_domain_name = module.s3_bucket.bucket_regional_domain_name
+  aliases = [
+    "cdn.${var.root_domain_name}",
+  ]
+  certificate_arn     = module.acm.certificate_arn
+  wait_for_deployment = true
+  pricing_class       = "PriceClass_100"
 }
 
 module "alb" {
@@ -125,11 +195,11 @@ module "rds" {
   backup_window           = "03:00-04:00"
   maintenance_window      = "Mon:00:00-Mon:03:00"
   backup_retention_period = 7
-  delete_protection       = false
-  skip_final_snapshot     = true
+  delete_protection       = false # for learning purposes, in production you should set this to true to prevent accidental deletion of the RDS instance
+  skip_final_snapshot     = true  # for learning purposes, in production you should set this to false and create a final snapshot
 
   #| High Availability
-  multi_az = false
+  multi_az = false # for learning purposes, in production you should set this to true to enable Multi-AZ deployments for high availability
 
   #| Monitoring
   rds_monitoring_role_arn        = module.iam.rds_monitoring_role_arn
@@ -184,16 +254,19 @@ module "ssm_parameter" {
       value       = var.database_password
       description = "The password for the database user."
     }
+
     database_username = {
       path        = "database/username"
       value       = var.database_username
       description = "The username for the database."
     }
+
     database_host = {
       path        = "database/host"
       value       = module.rds.db_endpoint
       description = "The endpoint of the RDS database."
     }
+
     database_port = {
       path        = "database/port"
       value       = module.rds.db_port
