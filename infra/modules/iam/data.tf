5a2592f6fc56a7d47f0e985130915acb96b3c1c6
deb-alien <dev.rahim936@gmail.com>
2026-07-14 21:40:29 +0600

    Refactor IAM policies and roles for ECS application, adding application policy document and restructuring role definitions.

--- infra/modules/iam/data.tf
+++ infra/modules/iam/data.tf
@@ -13,6 +13,58 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
   }
 }
 
+/**
+  ** Create an IAM policy document for the ECS application role.
+  ** This policy grants the ECS task permissions to access SSM parameters, KMS decryption, and S3 bucket operations.
+*/
+data "aws_iam_policy_document" "application" {
+  statement {
+    sid    = "SSMRead"
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath"
+    ]
+    resources = [
+      "arn:${data.aws_partition.current.partition}:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter${local.parameter_path}/*"
+    ]
+  }
+  statement {
+    sid    = "KMSDecrypt"
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt"
+    ]
+
+    resources = [data.aws_kms_key.ssm.arn]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+
+      values = [
+        "ssm.${data.aws_region.current.region}.amazonaws.com"
+      ]
+    }
+  }
+
+  statement {
+    sid       = "ApplicationS3Access"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = [var.s3_bucket_arn]
+  }
+
+  statement {
+    sid       = "ApplicationS3ObjectAccess"
+    effect    = "Allow"
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListMultipartUploadParts", "s3:AbortMultipartUpload"]
+    resources = ["${var.s3_bucket_arn}/*"]
+  }
+}
+
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 data "aws_region" "current" {}
--- infra/modules/iam/task_role.tf
+++
@@ -1,8 +0,0 @@
-/**
-** Create an IAM role for ECS tasks with the necessary trust relationship and tags.
-*/
-resource "aws_iam_role" "task" {
-  name               = local.task_role_name
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
-  tags               = merge(local.common_tags, { Name = local.task_role_name })
-}
