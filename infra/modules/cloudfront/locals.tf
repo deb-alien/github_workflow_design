20ba633cf771af5c83c41ba87f71aabd5951ffe4
deb-alien <dev.rahim936@gmail.com>
2026-07-14 23:30:19 +0600

    Add SSM parameters for CloudFront private key, public key ID, domain name, and alias

--- infra/modules/cloudfront/locals.tf
+++ infra/modules/cloudfront/locals.tf
@@ -6,4 +6,11 @@ locals {
     Module      = "Cloudfront"
   }
   parameter_prefix = "/${var.project_name}/${var.environment}"
+
+  ssm = {
+    cloudfront_alias            = "${local.parameter_prefix}/cloudfront/alias"
+    cloudfront_domain_name      = "${local.parameter_prefix}/cloudfront/domain_name"
+    cloudfront_public_key_id    = "${local.parameter_prefix}/cloudfront/public_key_id"
+    cloudfront_public_key_value = "${local.parameter_prefix}/cloudfront/public_key_value"
+  }
 }
--- infra/modules/cloudfront/ssm_parameter.tf
+++ infra/modules/cloudfront/ssm_parameter.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "cloudfront_private_key" {
-  name        = "${local.parameter_prefix}/cloudfront/private_key"
+resource "aws_ssm_parameter" "cloudfront_private_key_value" {
+  name        = local.ssm.cloudfront_private_key_value
   description = "The RSA 2048 private key used to sign CloudFront URLs"
   type        = "SecureString"
   value       = tls_private_key.this.private_key_pem
@@ -8,10 +8,28 @@ resource "aws_ssm_parameter" "cloudfront_private_key" {
 }
 
 resource "aws_ssm_parameter" "cloudfront_public_key_id" {
-  name        = "${local.parameter_prefix}/cloudfront/public_key_id"
+  name        = local.ssm.cloudfront_public_key_id
   description = "The ID of the CloudFront public key"
-  type        = "String"
+  type        = "SecureString"
   value       = aws_cloudfront_public_key.this.id
 
   tags = merge(local.common_tags, { Name = "CloudFront Public Key ID" })
+}
+
+resource "aws_ssm_parameter" "cloudfront_domain_name" {
+  name        = local.ssm.cloudfront_domain_name
+  description = "The domain name of the CloudFront distribution"
+  type        = "SecureString"
+  value       = aws_cloudfront_distribution.this.domain_name
+
+  tags = merge(local.common_tags, { Name = "CloudFront Domain Name" })
+}
+
+resource "aws_ssm_parameter" "cloudfront_alias" {
+  name        = local.ssm.cloudfront_alias
+  description = "The alias of the CloudFront distribution"
+  type        = "SecureString"
+  value       = values(var.aliases)[0]
+
+  tags = merge(local.common_tags, { Name = "CloudFront Alias" })
 }
