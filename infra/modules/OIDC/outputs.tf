cc08edda4a42b5181e2eb0a097286d7651bc2724
deb-alien <dev.rahim936@gmail.com>
2026-07-14 20:08:56 +0600

    Remove OIDC and ECR module files and update S3 module paths for consistency

--- infra/modules/OIDC/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "github_oidc_role_arn" {
-  value       = aws_iam_role.github_oidc_role.arn
-  description = "The ARN of the IAM role for GitHub Actions OIDC"
-}
-
-output "oidc_provider_arn" {
-  value       = aws_iam_openid_connect_provider.github_oidc.arn
-  description = "The ARN of the OIDC provider for GitHub Actions"
-}
--- infra/modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tf_state_bucket_name" {
-  value       = aws_s3_bucket.this.bucket
-  description = "The name of the S3 bucket for storing Terraform state files"
-}
-
-output "tf_state_bucket_arn" {
-  value       = aws_s3_bucket.this.arn
-  description = "The ARN of the S3 bucket for storing Terraform state files"
-}
