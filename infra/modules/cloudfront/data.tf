17f978b9ad2339d492a562cf3cbe321e43419e61
deb-alien <dev.rahim936@gmail.com>
2026-07-14 14:55:42 +0600

    feat(cloudfront): Update cache policy name to use origin cache control headers with query strings

--- infra/modules/cloudfront/data.tf
+++ infra/modules/cloudfront/data.tf
@@ -1,3 +1,3 @@
 data "aws_cloudfront_cache_policy" "managed_cache_policy_with_query_strings" {
-  name = "Managed-CachingOptimized-WithQueryStrings"
+  name = "UseOriginCacheControlHeaders-QueryStrings"
 }
