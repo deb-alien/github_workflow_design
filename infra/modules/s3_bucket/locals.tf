8de4cec3787c5b72c9c4a853225617e92e20faf3
deb-alien <dev.rahim936@gmail.com>
2026-07-14 18:32:10 +0600

    Refactor S3 bucket IAM policy to ensure ECS task role access is correctly defined and update bucket name assignment logic

--- infra/modules/s3_bucket/locals.tf
+++ infra/modules/s3_bucket/locals.tf
@@ -5,7 +5,7 @@ locals {
     ManagedBy   = "Terraform"
     module      = "S3 Bucket"
   }
-  bucket_name = var.bucket_name != "" ? var.bucket_name : "${var.project_name}-${var.environment}-storage"
+  bucket_name = var.bucket_name
 
 
   ecs_object_actions = [
