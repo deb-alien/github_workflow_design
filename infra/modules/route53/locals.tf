de96d32e558107ffb3debe25aff64bcb2153a6fb
deb-alien <dev.rahim936@gmail.com>
2026-07-15 21:26:40 +0600

    Refactor Route53 configuration: rename variables for clarity and consistency

--- infra/modules/route53/locals.tf
+++ infra/modules/route53/locals.tf
@@ -1,7 +1,15 @@
 locals {
-  record_name = (
+  api_record_name = (
     var.environment == "production"
     ? "${var.sub_domain}.${var.domain_name}"
     : "${var.environment}-${var.sub_domain}.${var.domain_name}"
   )
+
+  cdn_record_names = [
+    for cdn in var.cdn_domain_aliases : (
+      var.environment == "production"
+      ? cdn
+      : "${var.environment}-${cdn}"
+    )
+  ]
 }
