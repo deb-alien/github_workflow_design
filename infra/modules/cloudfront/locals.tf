9b8257c8b0ff05d06e5ae8c4f2333edcc898944f
deb-alien <dev.rahim936@gmail.com>
2026-07-15 14:37:02 +0600

    Refactor CloudFront SSM parameters to use a single resource block and structured locals

--- infra/modules/cloudfront/locals.tf
+++ infra/modules/cloudfront/locals.tf
@@ -8,9 +8,29 @@ locals {
   parameter_prefix = "/${var.project_name}/${var.environment}"
 
   ssm = {
-    cloudfront_alias            = "${local.parameter_prefix}/cloudfront/alias"
-    cloudfront_domain_name      = "${local.parameter_prefix}/cloudfront/domain_name"
-    cloudfront_public_key_id    = "${local.parameter_prefix}/cloudfront/public_key_id"
-    cloudfront_public_key_value = "${local.parameter_prefix}/cloudfront/public_key_value"
+    cloudfront_alias = {
+      name        = "${local.parameter_prefix}/cloudfront/alias"
+      type        = "String"
+      description = "The alias of the CloudFront distribution"
+      value       = var.aliases[0]
+    }
+    cloudfront_domain_name = {
+      name        = "${local.parameter_prefix}/cloudfront/domain_name"
+      type        = "String"
+      description = "The domain name of the CloudFront distribution"
+      value       = aws_cloudfront_distribution.this.domain_name
+    }
+    cloudfront_public_key_id = {
+      name        = "${local.parameter_prefix}/cloudfront/public_key_id"
+      type        = "SecureString"
+      description = "The ID of the CloudFront public key"
+      value       = aws_cloudfront_public_key.this.id
+    }
+    cloudfront_public_key_value = {
+      name        = "${local.parameter_prefix}/cloudfront/public_key_value"
+      type        = "SecureString"
+      description = "The RSA 2048 private key used to sign CloudFront URLs"
+      value       = tls_private_key.this.private_key_pem
+    }
   }
 }
--- infra/modules/cloudfront/ssm_parameter.tf
+++ infra/modules/cloudfront/ssm_parameter.tf
@@ -1,35 +1,10 @@
-resource "aws_ssm_parameter" "cloudfront_private_key_value" {
-  name        = local.ssm.cloudfront_public_key_value
-  description = "The RSA 2048 private key used to sign CloudFront URLs"
-  type        = "SecureString"
-  value       = tls_private_key.this.private_key_pem
+resource "aws_ssm_parameter" "this" {
+  for_each = local.ssm
 
-  tags = merge(local.common_tags, { Name = "CloudFront Private Key" })
-}
+  name        = each.value.name
+  description = each.value.description
+  type        = each.value.type
+  value       = each.value.value
 
-resource "aws_ssm_parameter" "cloudfront_public_key_id" {
-  name        = local.ssm.cloudfront_public_key_id
-  description = "The ID of the CloudFront public key"
-  type        = "SecureString"
-  value       = aws_cloudfront_public_key.this.id
-
-  tags = merge(local.common_tags, { Name = "CloudFront Public Key ID" })
-}
-
-resource "aws_ssm_parameter" "cloudfront_domain_name" {
-  name        = local.ssm.cloudfront_domain_name
-  description = "The domain name of the CloudFront distribution"
-  type        = "SecureString"
-  value       = aws_cloudfront_distribution.this.domain_name
-
-  tags = merge(local.common_tags, { Name = "CloudFront Domain Name" })
-}
-
-resource "aws_ssm_parameter" "cloudfront_alias" {
-  name        = local.ssm.cloudfront_alias
-  description = "The alias of the CloudFront distribution"
-  type        = "SecureString"
-  value       = var.aliases[0]
-
-  tags = merge(local.common_tags, { Name = "CloudFront Alias" })
+  tags = local.common_tags
 }
