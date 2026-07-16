03541381b70c7f11c34c51d421906abc52b9004a
deb-alien <dev.rahim936@gmail.com>
2026-07-16 06:15:24 +0600

    Refactor ECS deploy workflow: streamline steps, remove unused scripts, and enhance rollback functionality

--- .github/workflows/actions/ecs-deploy/scripts/register-task-definition.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-aws ecs register-task-definition \
-    --cli-input-json file://"$1" \
-    --query 'taskDefinition.taskDefinitionArn' \
-    --output text
