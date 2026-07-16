b1dbbaedbc83b7b21e5d4245337be3f1f3288a15
Rahim-ul Islam Alin <dev.rahim936@gmail.com>
2026-07-16 06:21:04 +0600

    Refactor ECS deploy workflow and enhance rollback functionality (#67)
    * Refactor ECS deploy workflow: streamline steps, remove unused scripts, and enhance rollback functionality
    
    * Reorder parameters in ECS deploy step and enable rollback on failure

--- .github/workflows/actions/ecs-deploy/scripts/rollback.sh
+++ .github/workflows/actions/ecs-deploy/scripts/rollback.sh
@@ -4,14 +4,18 @@ set -euo pipefail
 
 CLUSTER="$1"
 SERVICE="$2"
-TASK="$3"
+TASK_DEFINITION="$3"
+
+echo "Deployment failed."
+echo "Rolling back..."
 
 aws ecs update-service \
     --cluster "$CLUSTER" \
     --service "$SERVICE" \
-    --task-definition "$TASK" \
-    >/dev/null
+    --task-definition "$TASK_DEFINITION"
 
 aws ecs wait services-stable \
     --cluster "$CLUSTER" \
     --services "$SERVICE"
+
+echo "Rollback complete."
