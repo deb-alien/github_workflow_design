325bb2e39daea9ea32bddaaf36e1589235dd7d03
deb-alien <dev.rahim936@gmail.com>
2026-07-17 15:27:52 +0600

    Refactor CloudFront locals and outputs to enhance descriptions and add missing outputs

--- infra/modules/cloudfront/locals.tf
+++ infra/modules/cloudfront/locals.tf
@@ -11,25 +11,25 @@ locals {
     cloudfront_alias = {
       name        = "${local.parameter_prefix}/cloudfront/alias"
       type        = "String"
-      description = "The alias of the CloudFront distribution"
+      description = "The target public alias mapped to the CloudFront distribution"
       value       = var.aliases[0]
     }
     cloudfront_domain_name = {
       name        = "${local.parameter_prefix}/cloudfront/domain_name"
       type        = "String"
-      description = "The domain name of the CloudFront distribution"
+      description = "The auto-generated AWS canonical domain name of the CloudFront distribution"
       value       = aws_cloudfront_distribution.this.domain_name
     }
     cloudfront_public_key_id = {
       name        = "${local.parameter_prefix}/cloudfront/public_key_id"
       type        = "String"
-      description = "The ID of the CloudFront public key"
+      description = "The public identifier used to map trusted signer keys to signed URL generators"
       value       = aws_cloudfront_public_key.this.id
     }
     cloudfront_private_key = {
       name        = "${local.parameter_prefix}/cloudfront/private_key"
       type        = "SecureString"
-      description = "The RSA 2048 private key used to sign CloudFront URLs"
+      description = "The RSA 2048 private key used to sign secure CloudFront asset request URLs"
       value       = tls_private_key.this.private_key_pem
     }
   }
